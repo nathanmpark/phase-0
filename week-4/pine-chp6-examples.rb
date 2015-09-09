eaa657c4f9d0d48aae8b1bbba91a1c76793fe514
Nathan Park <nate.park90@gmail.com>
2015-09-09 01:26:04 -0700

    Add pine chapter 6 examples, address challenge, etc

---
+++ week-4/pine-chp6-examples.rb
@@ -0,0 +1,13 @@
+#Angry Boss
+# puts "WHAT DO YOU WANT?"
+# answer = gets.chomp
+# puts "DID YOU JUST SAY " + answer + "? WTF IS WRONG WITH YOU?!?!?!?"
+
+#Table of Contents
+width = 50
+puts "Table of Contents".center(width)
+puts
+puts "Chapter 1: Getting Started".ljust(width/2) + "page  1".rjust((width-1)/2)
+puts "Chapter 2: Numbers".ljust((width+1)/2) + "page  9".rjust(width/2)
+puts "Chapter 3: Letters".ljust((width+1)/2) + "page 13".rjust(width/2)
+
