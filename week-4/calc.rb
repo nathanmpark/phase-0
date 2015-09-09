8aed32e7c77c4b5a3778f192a817d0f4bc83638b
Nathan Park <nate.park90@gmail.com>
2015-09-08 17:17:26 -0700

    Add ruby exercises

---
+++ week-4/calc.rb
@@ -0,0 +1,26 @@
+# puts 1+2
+
+# puts 1.0 + 2.0
+# puts 2.0 * 3.0
+# puts 5.0 - 8.0
+# puts 9.0 / 2.0
+
+# puts 1+2
+# puts 2*3
+# puts 5-8
+# puts 9/2
+
+# puts 5*(12-8)+ -15
+# puts 98+(59872/(13*8)) * -51
+
+# Hours in a year
+puts 24*365
+
+# Minutes in a decade
+puts 60*24*365*10
+
+# Your age in seconds
+puts 60*60*24*365*25
+
+# Our dear author's age
+puts 1160000000/(60*60*24*365)
