618ea96ac79de4bcac23e85234f71cdbdb40dc0f
Nathan Park <nate.park90@gmail.com>
2015-09-09 01:50:18 -0700

    Add reflections

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -17,3 +17,23 @@ puts "What is your favorite number?"
 num = gets.chomp
 num = num.to_i + 1
 puts "Your new favorite number is " + num.to_s
+
+=begin
+How do you define a local variable?
+  A local variable is assigned using the "=" sign. You can assign a variable a number or a string.
+
+How do you define a method?
+  A [method](./define-method/my_solution.rb) is a local function, which runs a series of operations upon input variables.
+
+What is the difference between a local variable and a method?
+  A variable stores a value, while a method will operate some sort of function.
+
+How do you run a ruby program from the command line?
+  By writing "ruby [file.rb]".
+
+How do you run an RSpec file from the command line?
+  By writing "rspec [file_spec.rb]".
+
+What was confusing about this material? What made sense?
+  There was a portion of the material that was confusing when taking in a number as a string, converting it to an integer to perform basic math functions, then returning it to a string to print out to the console. What made sense were the built in methods and the syntax of ruby methods that make sense like ".length".
+=end
