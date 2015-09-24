292db4e658cbb484ecb096074492b76942a8a06c
Nathan Park <nate.park90@gmail.com>
2015-09-23 18:10:05 -0700

    Add gps 2.3 reflection comments, guessing game reflection, and nested_data_solution file.

--- week-6/guessing-game/my_solution.rb
+++ week-6/guessing-game/my_solution.rb
@@ -74,9 +74,13 @@ end
 # Reflection
 
 # How do instance variables and methods represent the characteristics and behaviors (actions) of a real-world object?
+# Instance variables are useful in that they can be utilized across methods within a class. This mirrors the characteristic of a given object in the real-world retaining its value regardless of where it might be referenced. For example, a 100 bill will always keeps its value regardless of how it is used or passed (it can be change, used to purchase items, or deposited into a bank). No matter how it is used, it retains its value.
 
 # When should you use instance variables? What do they do for you?
+# Instance variables should be used when creating a class, allowing for variables to be passed between methods. They allows for something more substantive than local variables which are limited to within the method.
 
 # Explain how to use flow control. Did you have any trouble using it in this challenge? If so, what did you struggle with?
+# Flow control in that you can translate your logic into blocks of code, most commonly in my code as if/else statements. I didn't have any problems, just refactored some code using unless.
 
 # Why do you think this code requires you to return symbols? What are the benefits of using symbols?
+# It is faster and easier to use symbols, in that the machine doesn't have to translate a string and print out the statement. As a symbol, the runtime is faster and it is unique and immutable.
