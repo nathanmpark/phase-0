1c792c743323d754fda1bf56acd81ae708cd1423
Nathan Park <nate.park90@gmail.com>
2015-09-29 15:25:55 -0700

    add group project

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,15 @@
+// Release 1: Test to Users Stories
+
+As a user, I want to have three different functions which calculate the sum, mean, and median for a collection of numbers.
+As a user, I want all three functions to work regardless of there being even or odd number of elements within the collection of numbers.
+As a user, I want all three mathematical functions to use decimals, and not round any numbers.
+As a user, I want all three functions to handle collections of numbers with repeated elements.
+
+As a user, I want the first function to be called "sum".
+As a user, I want the "sum" function to return the sum of all elements within the collection of numbers.
+
+As a user, I want the second function to be called "mean".
+As a user, I want the "mean" function to return the average of all the elements within the collection of numbers.
+
+As a user, I was the third function to be called "median".
+As a user, I want the "median" function to return the median or middle value of all the elements in the collection of numbers.
